77869416938ddbb525b5339891158009b6ac268a
man7788 <jerman1841@gmail.com>
2022-10-21 02:45:51 +0800

    Add click to display marks in grids

--- javascript.js
+++ javascript.js
@@ -3,13 +3,25 @@ const gameBoard = (() => {
   const boardGrid = document.querySelectorAll('.game-grid');
   const renderGrid = () => {
     for (i = 0; i < boardGrid.length; i++) {
-      boardGrid[i].textContent = 'X';
+      boardGrid[i].addEventListener('click', displayGrid);
     }
   };
+  let displayCount = 1;
+  const displayGrid = (e) => {
+    displayCount += 1;
+    gridNum = e.target.id;
+    gridNum = gridNum.slice(-1);
+    if (displayCount % 2 === 0) {
+      boardGrid[gridNum].textContent = 'X';
+      } else {
+        boardGrid[gridNum].textContent = 'O';
+      }
+    boardGrid[gridNum].removeEventListener('click', displayGrid);
+  }
   return { renderGrid };
 })();
 
-let displayController = (() => {
+const displayController = (() => {
 
 })();
 
@@ -18,3 +30,8 @@ const playerFactory = (playerName) => {
 };
 
 gameBoard.renderGrid();
+
+// Add click feature to each grid
+// Make alternative X and O
+// Render grid to display new mark
+// Turn off click feature of marked grid
